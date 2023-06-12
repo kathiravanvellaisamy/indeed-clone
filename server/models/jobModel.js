42c331c216c1e54ce039787ee3468256d430260a
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2023-06-12 13:19:08 +0530

    created job model using mongoose

---
+++ server/models/jobModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const jobSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a Job Title"],
+    },
+    company: {
+      type: String,
+      required: [true, "Please add a Company"],
+    },
+    city: {
+      type: String,
+      required: [true, "Please add a City"],
+    },
+    salary: {
+      type: String,
+      required: [true, "Please add a Salary"],
+    },
+    description: {
+      type: te,
+      required: [true, "Please add a description"],
+    },
+    Email: {
+      type: String,
+      required: [true, "Please add a Contact Email"],
+    },
+    website: {
+      type: String,
+      required: [true, "Please add a Website URL"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Please add a Phone Number"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Joblist", jobSchema);
