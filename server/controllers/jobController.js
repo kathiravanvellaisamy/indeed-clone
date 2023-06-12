9361e71957277f684a1ae2dd394ed3b83dc9a33c
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2023-06-12 12:48:23 +0530

    added error middleware and ex-async-handler

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,48 +1,62 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc GET All Jobs list
 // @route GET /api/joblisting
 // @access Public
 
-const getJobLists = (req, res) => {
+const getJobLists = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all the Job listings..." });
-};
+});
 
 // @desc GET Particular Job list
 // @route GET /api/joblisting/:id
 // @access Public
 
-const getJob = (req, res) => {
+const getJob = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Job Id ${req.params.id} shows the details` });
-};
+});
 
 // @desc Post a Jobs list
 // @route POST /api/joblisting
 // @access Private
 
-const postJob = (req, res) => {
+const postJob = asyncHandler(async (req, res) => {
+  if (!req.body.jobtitle) {
+    res.status(400);
+    throw new Error("Please enter a Job Title");
+  }
+  if (!req.body.location) {
+    res.status(400);
+    throw new Error("Please enter a Location");
+  }
+  if (!req.body.salary) {
+    res.status(400);
+    throw new Error("Please enter a Salary");
+  }
   res.status(200).json({ message: "Job Posted Successfully!" });
-};
+});
 
 // @desc Update the Jobs list
 // @route PUT /api/joblisting/:id
 // @access Private
 
-const updateJob = (req, res) => {
+const updateJob = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Job Id ${req.params.id} Updated Successfully!` });
-};
+});
 
 // @desc Delete the  Jobs list
 // @route DELETE /api/joblisting/:id
 // @access Private
 
-const deleteJob = (req, res) => {
+const deleteJob = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Job Id ${req.params.id} Deleted Successfully!` });
-};
+});
 
 module.exports = {
   getJobLists,
--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+//middlwares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
 
 app.use("/api/joblisting", require("./routes/jobRoutes"));
 
