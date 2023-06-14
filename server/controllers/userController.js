a61dcccbe563640d434678ebc3d624d6b5d310b1
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2023-06-14 22:43:19 +0530

    created register anf login logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,26 +1,78 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
 // @desc Register new User
 // @route POST /api/users
 // @access Public
 
-const registerUser = (req, res) => {
-  res.json({ message: "Register User" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+  //Check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hashed Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data!");
+  }
+});
 
 // @desc Authenticate a User
 // @route POST /api/users/login
 // @access Public
 
-const loginUser = (req, res) => {
-  res.json({ message: "Login User" });
-};
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  //Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials!");
+  }
+});
 
 // @desc Get User data
 // @route GET /api/users/dashboard
 // @access Public
 
-const dashBoard = (req, res) => {
+const dashBoard = asyncHandler((req, res) => {
   res.json({ message: "Users Dashboard" });
-};
+});
 
 module.exports = {
   registerUser,
