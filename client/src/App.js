d00657ac7db4fd114bab758b67645386a72b45f4
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2023-06-16 09:59:32 +0530

    Initialize frontend with react & redux

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-12">
+      <h1 className="text-zinc-800 text-9xl font-extralight">Job Portal</h1>
+    </div>
+  );
+}
+
+export default App;
